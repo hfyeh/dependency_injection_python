e0c240626fa60ac6b516f72c205209175595db06
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-07-26 01:02:56 +0800

    Add get_password

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -1,3 +1,8 @@
+from .user import User
+
 class AuthenticationService:
     def verify(self, username: str, password: str, otp: str) -> bool:
         raise NotImplementedError()
+
+    def get_password(self, username: str) -> str:
+        return User.query.filter_by(username=username).first().password
