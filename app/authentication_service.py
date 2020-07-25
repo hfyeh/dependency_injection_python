6580feba2a8194ac4755e374377c679e35940408
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-07-26 01:13:16 +0800

    Add get_hash

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -1,6 +1,13 @@
 from .user import User
+import hashlib
 
 class AuthenticationService:
     def verify(self, username: str, password: str, otp: str) -> bool:
         password_from_db = User.query.filter_by(username=username).first().password
         raise NotImplementedError()
+
+    def get_hash(self, plain_text: str) -> str:
+        crypt = hashlib.sha256()
+        crypt.update(plain_text)
+        hash = crypt.hexdigest()
+        return hash
