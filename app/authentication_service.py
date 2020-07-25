ae21d889e656122470501980dc03dae0b9fdc709
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-07-26 01:07:11 +0800

    Inline get_password

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -2,7 +2,5 @@ from .user import User
 
 class AuthenticationService:
     def verify(self, username: str, password: str, otp: str) -> bool:
+        password_from_db = User.query.filter_by(username=username).first().password
         raise NotImplementedError()
-
-    def get_password(self, username: str) -> str:
-        return User.query.filter_by(username=username).first().password
