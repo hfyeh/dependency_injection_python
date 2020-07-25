e43365e2e86b275cb516421f651819f92ae0f7ce
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-07-26 01:40:45 +0800

    Inline get_otp

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -12,12 +12,11 @@ class AuthenticationService:
         hash = crypt.hexdigest()
         hashed_password = hash
 
-        raise NotImplementedError()
-
-    def get_otp(self, username: str) -> str:
         response = requests.post('https://sharefun.com/api/otp', data={username: username})
         if response.status_code == requests.codes.ok:
-            otp = response.json()['otp']
+            otp1 = response.json()['otp']
         else:
             raise PermissionError()
-        return otp
+        current_otp = otp1
+
+        raise NotImplementedError()
