5d63a53695efa4a5e68cde6609cbbb42d148dc92
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 02:44:41 +0800

    Extract constant DefaultHashedPassword

--- test/authentication_service_test.py
+++ test/authentication_service_test.py
@@ -4,6 +4,8 @@ from unittest.mock import create_autospec
 
 from app import AuthenticationService
 
+DefaultHashedPassword = 'hashed_password'
+
 DefaultOtp = 'current_otp'
 
 DefaultPassword = '123456'
@@ -30,8 +32,8 @@ class AuthenticationServiceTest(unittest.TestCase):
     def test_is_valid(self):
         self._failed_counter.is_account_locked = create_autospec(self._failed_counter.is_account_locked,
                                                                  return_value=False)
-        self._user.get_password = create_autospec(self._user.get_password, return_value='hashed_password')
-        self._hash.compute = create_autospec(self._hash.compute, return_value='hashed_password')
+        self._user.get_password = create_autospec(self._user.get_password, return_value=DefaultHashedPassword)
+        self._hash.compute = create_autospec(self._hash.compute, return_value=DefaultHashedPassword)
         self._otp_service.get_current_otp = create_autospec(self._otp_service.get_current_otp,
                                                             return_value=DefaultOtp)
 
