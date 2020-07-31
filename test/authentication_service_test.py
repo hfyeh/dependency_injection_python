60b2b3efe17e67da0eb8262f1949c85b641b7b03
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 02:44:18 +0800

    Extract constant DefaultOtp

--- test/authentication_service_test.py
+++ test/authentication_service_test.py
@@ -4,6 +4,8 @@ from unittest.mock import create_autospec
 
 from app import AuthenticationService
 
+DefaultOtp = 'current_otp'
+
 DefaultPassword = '123456'
 
 DefaultUsername = 'sharefun'
@@ -31,9 +33,9 @@ class AuthenticationServiceTest(unittest.TestCase):
         self._user.get_password = create_autospec(self._user.get_password, return_value='hashed_password')
         self._hash.compute = create_autospec(self._hash.compute, return_value='hashed_password')
         self._otp_service.get_current_otp = create_autospec(self._otp_service.get_current_otp,
-                                                            return_value='current_otp')
+                                                            return_value=DefaultOtp)
 
-        is_valid = self._authentication_service.verify(DefaultUsername, DefaultPassword, 'current_otp')
+        is_valid = self._authentication_service.verify(DefaultUsername, DefaultPassword, DefaultOtp)
         self.assertTrue(is_valid)
 
 
