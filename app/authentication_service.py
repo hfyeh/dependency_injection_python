febda2f8cf9a940a44b485247d813690fc64dd81
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-07-31 21:12:41 +0800

    Extract class OtpService

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -8,10 +8,20 @@ from slack.errors import SlackApiError
 import logging
 
 
+class OtpService:
+    def get_current_otp(self, username: str) -> str:
+        response = requests.post('https://sharefun.com/api/otp', data={username: username})
+        if not (response.status_code == requests.codes.ok):
+            raise PermissionError()
+        current_otp = response.json()['otp']
+        return current_otp
+
+
 class AuthenticationService:
     def __init__(self):
         self._user: User = User()
         self._sha_256_adapter: Sha256Adapter = Sha256Adapter()
+        self._otp_service: OtpService = OtpService()
 
     def verify(self, username: str, password: str, otp: str) -> bool:
         if self.is_account_locked(username):
@@ -21,7 +31,7 @@ class AuthenticationService:
 
         hashed_password = self._sha_256_adapter.compute_hashed_password(password)
 
-        current_otp = self.get_current_otp(username)
+        current_otp = self._otp_service.get_current_otp(username)
 
         if password_from_db == hashed_password and otp == current_otp:
             self.reset_failed_count(username)
@@ -66,13 +76,6 @@ class AuthenticationService:
     def reset_failed_count(self, username: str) -> None:
         response = requests.post('https://sharefun.com/api/failed_counter/reset', data={username: username})
         response.raise_for_status()
-
-    def get_current_otp(self, username: str) -> str:
-        response = requests.post('https://sharefun.com/api/otp', data={username: username})
-        if not (response.status_code == requests.codes.ok):
-            raise PermissionError()
-        current_otp = response.json()['otp']
-        return current_otp
 
 
 class FailedTooManyTimesError(OSError):
