6e57fb028bae1f6e2f5229365c59c40df789c7d7
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 00:41:26 +0800

    Extract interface ILogging from Logging

--- app/logging.py
+++ app/logging.py
@@ -1,6 +1,13 @@
 import logging
+from abc import ABCMeta, abstractmethod
 
 
-class Logging:
+class ILogging(metaclass=ABCMeta):
+    @abstractmethod
+    def log_failed_count(self, message):
+        pass
+
+
+class Logging(ILogging):
     def log_failed_count(self, message) -> None:
         logging.info(message)
