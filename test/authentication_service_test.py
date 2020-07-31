c94d9e0fecd98d80c028464ddb35db83e196b53c
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 02:48:57 +0800

    Extract parameter 'password' of '_given_password'

--- test/authentication_service_test.py
+++ test/authentication_service_test.py
@@ -31,7 +31,7 @@ class AuthenticationServiceTest(unittest.TestCase):
 
     def test_is_valid(self):
         self._given_account_is_locked(False)
-        self._given_password()
+        self._given_password(DefaultHashedPassword)
         self._hash.compute = create_autospec(self._hash.compute, return_value=DefaultHashedPassword)
         self._otp_service.get_current_otp = create_autospec(self._otp_service.get_current_otp,
                                                             return_value=DefaultOtp)
@@ -39,8 +39,8 @@ class AuthenticationServiceTest(unittest.TestCase):
         is_valid = self._authentication_service.verify(DefaultUsername, DefaultPassword, DefaultOtp)
         self.assertTrue(is_valid)
 
-    def _given_password(self):
-        self._user.get_password = create_autospec(self._user.get_password, return_value=DefaultHashedPassword)
+    def _given_password(self, password):
+        self._user.get_password = create_autospec(self._user.get_password, return_value=password)
 
     def _given_account_is_locked(self, is_locked):
         self._failed_counter.is_account_locked = create_autospec(self._failed_counter.is_account_locked,
