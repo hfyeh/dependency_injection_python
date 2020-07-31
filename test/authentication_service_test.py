aa12a126584b02ccced5c988ec043ad13801acfa
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 01:10:50 +0800

    Stub user, hash, otp_service, failed_counter to pass the test

--- test/authentication_service_test.py
+++ test/authentication_service_test.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest import mock
+from unittest.mock import create_autospec
 
 from app import AuthenticationService
 
@@ -12,6 +13,11 @@ class AuthenticationServiceTest(unittest.TestCase):
         failed_counter = mock.Mock()
         notification = mock.Mock()
         logging = mock.Mock()
+
+        failed_counter.is_account_locked = create_autospec(failed_counter.is_account_locked, return_value=False)
+        user.get_password = create_autospec(user.get_password, return_value='hashed_password')
+        hash.compute = create_autospec(hash.compute, return_value='hashed_password')
+        otp_service.get_current_otp = create_autospec(otp_service.get_current_otp, return_value='current_otp')
 
         authentication_service = AuthenticationService(user,
                                                        hash,
