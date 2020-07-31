8b92eb874daf104d9fb806f7ac62ab04d9b5d325
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 02:41:52 +0800

    Extract fields and initialized in setUp

--- test/authentication_service_test.py
+++ test/authentication_service_test.py
@@ -6,27 +6,30 @@ from app import AuthenticationService
 
 
 class AuthenticationServiceTest(unittest.TestCase):
+    def setUp(self):
+        self._logging = mock.Mock()
+        self._notification = mock.Mock()
+        self._failed_counter = mock.Mock()
+        self._otp_service = mock.Mock()
+        self._hash = mock.Mock()
+        self._user = mock.Mock()
+
+        self._authentication_service = AuthenticationService(self._user,
+                                                             self._hash,
+                                                             self._otp_service,
+                                                             self._failed_counter,
+                                                             self._notification,
+                                                             self._logging)
+
     def test_is_valid(self):
-        user = mock.Mock()
-        hash = mock.Mock()
-        otp_service = mock.Mock()
-        failed_counter = mock.Mock()
-        notification = mock.Mock()
-        logging = mock.Mock()
+        self._failed_counter.is_account_locked = create_autospec(self._failed_counter.is_account_locked,
+                                                                 return_value=False)
+        self._user.get_password = create_autospec(self._user.get_password, return_value='hashed_password')
+        self._hash.compute = create_autospec(self._hash.compute, return_value='hashed_password')
+        self._otp_service.get_current_otp = create_autospec(self._otp_service.get_current_otp,
+                                                            return_value='current_otp')
 
-        failed_counter.is_account_locked = create_autospec(failed_counter.is_account_locked, return_value=False)
-        user.get_password = create_autospec(user.get_password, return_value='hashed_password')
-        hash.compute = create_autospec(hash.compute, return_value='hashed_password')
-        otp_service.get_current_otp = create_autospec(otp_service.get_current_otp, return_value='current_otp')
-
-        authentication_service = AuthenticationService(user,
-                                                       hash,
-                                                       otp_service,
-                                                       failed_counter,
-                                                       notification,
-                                                       logging)
-
-        is_valid = authentication_service.verify('sharefun', '123456', 'current_otp')
+        is_valid = self._authentication_service.verify('sharefun', '123456', 'current_otp')
         self.assertTrue(is_valid)
 
 
