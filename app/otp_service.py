99e2f7974013596e964afbdac1070a268fe1fcb2
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 00:30:27 +0800

    Extract interface IOtpService from OtpService

--- app/otp_service.py
+++ app/otp_service.py
@@ -1,7 +1,15 @@
+from abc import ABCMeta, abstractmethod
+
 import requests
 
 
-class OtpService:
+class IOtpService(metaclass=ABCMeta):
+    @abstractmethod
+    def get_current_otp(self, username: str) -> str:
+        pass
+
+
+class OtpService(IOtpService):
     def get_current_otp(self, username: str) -> str:
         response = requests.post('https://sharefun.com/api/otp', data={username: username})
         if not (response.status_code == requests.codes.ok):
