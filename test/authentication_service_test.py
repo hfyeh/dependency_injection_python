6ec379a7f3dd70149fc2759897ddeb1b03b1cbc3
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-01 01:04:19 +0800

    Complete test case is_valid by blackbox

--- test/authentication_service_test.py
+++ test/authentication_service_test.py
@@ -1,9 +1,27 @@
 import unittest
+from unittest import mock
+
+from app import AuthenticationService
 
 
 class AuthenticationServiceTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_is_valid(self):
+        user = mock.Mock()
+        hash = mock.Mock()
+        otp_service = mock.Mock()
+        failed_counter = mock.Mock()
+        notification = mock.Mock()
+        logging = mock.Mock()
+
+        authentication_service = AuthenticationService(user,
+                                                       hash,
+                                                       otp_service,
+                                                       failed_counter,
+                                                       notification,
+                                                       logging)
+
+        is_valid = authentication_service.verify('sharefun', '123456', 'current_otp')
+        self.assertTrue(is_valid)
 
 
 if __name__ == '__main__':
