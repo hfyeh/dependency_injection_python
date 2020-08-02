7bb898fc0129f81241809d66e8697655cd6aa88d
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-02 16:43:15 +0800

    Extract class LoggingDecorator

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -21,6 +21,18 @@ class AuthenticationBaseDecorator(IAuthenticationService):
         return self._authentication_service.verify(username, password, otp)
 
 
+class LoggingDecorator(AuthenticationBaseDecorator):
+    def __init__(self, authentication_service: IAuthenticationService, logging: ILogging,
+                 failed_counter: IFailedCounter):
+        super().__init__(authentication_service)
+        self._logging = logging
+        self._failed_counter = failed_counter
+
+    def log_message(self, username):
+        failed_count = self._failed_counter.get(username)
+        self._logging.info(f'user: {username} failed times: {failed_count}')
+
+
 class AuthenticationService(IAuthenticationService):
     def __init__(self, user: IUser = User(), hash: IHash = Sha256Adapter(), otp_service: IOtpService = OtpService(),
                  failed_counter: IFailedCounter = FailedCounter(), logging: ILogging = Logging()):
@@ -30,6 +42,7 @@ class AuthenticationService(IAuthenticationService):
         self._failed_counter: IFailedCounter = failed_counter
         self._logging: ILogging = logging
         # self._failed_counter_decorator = FailedCounterDecorator(self, failed_counter)
+        self._logging_decorator = LoggingDecorator(self, logging, failed_counter)
 
     def verify(self, username: str, password: str, otp: str) -> bool:
         # self._failed_counter_decorator.check_account_is_locked(username)
@@ -43,13 +56,9 @@ class AuthenticationService(IAuthenticationService):
         if password_from_db == hashed_password and otp == current_otp:
             return True
         else:
-            self.log_message(username)
+            self._logging_decorator.log_message(username)
 
             return False
-
-    def log_message(self, username):
-        failed_count = self._failed_counter.get(username)
-        self._logging.info(f'user: {username} failed times: {failed_count}')
 
 
 class FailedTooManyTimesError(OSError):
