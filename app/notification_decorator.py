3732c324bda237d989e5d74cc66f5370c4f50ffb
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-02 15:01:44 +0800

    Extract base class AuthenticationBaseDecorator

--- app/notification_decorator.py
+++ app/notification_decorator.py
@@ -2,9 +2,14 @@ from app import INotification
 from app.authentication_service import IAuthenticationService
 
 
-class NotificationDecorator(IAuthenticationService):
-    def __init__(self, authentication_service: IAuthenticationService, notification: INotification):
+class AuthenticationBaseDecorator:
+    def __init__(self, authentication_service: IAuthenticationService):
         self._authentication_service = authentication_service
+
+
+class NotificationDecorator(AuthenticationBaseDecorator, IAuthenticationService):
+    def __init__(self, authentication_service: IAuthenticationService, notification: INotification):
+        super().__init__(authentication_service)
         self._notification = notification
 
     def notify(self, username):
