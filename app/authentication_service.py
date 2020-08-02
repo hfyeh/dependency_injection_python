e2d36964bc4986a59a78528cccb039992574ed87
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2020-08-02 16:14:19 +0800

    Clean up Notification from AuthenticationService

--- app/authentication_service.py
+++ app/authentication_service.py
@@ -2,10 +2,8 @@ from abc import ABCMeta, abstractmethod
 
 from .failed_counter import FailedCounter, IFailedCounter
 from .logging import Logging, ILogging
-# from .notification_decorator import NotificationDecorator
 from .otp_service import OtpService, IOtpService
 from .sha_256_adapter import Sha256Adapter, IHash
-from .slack_adapter import SlackAdapter, INotification
 from .user import User, IUser
 
 
@@ -25,15 +23,12 @@ class AuthenticationBaseDecorator(IAuthenticationService):
 
 class AuthenticationService(IAuthenticationService):
     def __init__(self, user: IUser = User(), hash: IHash = Sha256Adapter(), otp_service: IOtpService = OtpService(),
-                 failed_counter: IFailedCounter = FailedCounter(), notification: INotification = SlackAdapter(),
-                 logging: ILogging = Logging()):
+                 failed_counter: IFailedCounter = FailedCounter(), logging: ILogging = Logging()):
         self._user: IUser = user
         self._hash: IHash = hash
         self._otp_service: IOtpService = otp_service
         self._failed_counter: IFailedCounter = failed_counter
-        # self._notification: INotification = notification
         self._logging: ILogging = logging
-        # self._notification_decorator = NotificationDecorator(self, notification)
 
     def verify(self, username: str, password: str, otp: str) -> bool:
         if self._failed_counter.is_account_locked(username):
@@ -51,8 +46,6 @@ class AuthenticationService(IAuthenticationService):
             return True
         else:
             self._failed_counter.add(username)
-
-            # self._notification_decorator.notify(username)
 
             failed_count = self._failed_counter.get(username)
             self._logging.info(f'user: {username} failed times: {failed_count}')
